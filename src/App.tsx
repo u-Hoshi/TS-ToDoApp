ac6b2612e3bdf56c9ae7315b002fd8f4bc7f7aff
u-Hoshi <yuta0000707@yahoo.co.jp>
2021-05-15 17:55:13 +0900

    タスクの追加機能を実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,56 @@
-import React,{VFC,useState,useEffect} from 'react';
+import React, { VFC, useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {db} from "./firebase"
+import { db } from './firebase';
 import { UnsubscribeTwoTone } from '@material-ui/icons';
+import { FormControl, TextField } from '@material-ui/core';
+import AddToPhotosIcon from '@material-ui/icons/AddToPhotos';
 
-const App:VFC=() =>{
-  const [tasks,setTask] = useState([{id:"",title:""}])
+const App: VFC = () => {
+  const [tasks, setTask] = useState([{ id: '', title: '' }]);
+  const [input, setInput] = useState<String>('');
 
-useEffect(()=>{
-  const unSub =db.collection("task").onSnapshot((snapshot)=>{
-    setTask(
-      snapshot.docs.map((doc)=>(
-        {id:doc.id,title:doc.data().title}
-      ))
-    )
-  })
-  //  クリーンナップ関数
-  return ()=>unSub
-},[])
+  console.log(!input);
+
+  // 初回レンダリング時に取得して画面に描画
+  useEffect(() => {
+    const unSub = db.collection('tasks').onSnapshot((snapshot) => {
+      setTask(
+        snapshot.docs.map((doc) => ({ id: doc.id, title: doc.data().title }))
+      );
+    });
+    //  クリーンナップ関数
+    return () => unSub();
+  }, []);
+
+  const NewTask: VFC = (e: React.MouseEventHandler<HTMLButtonElement>) => {
+    db.collection('tasks').add({ title: input });
+    setInput('');
+  };
 
   return (
-    <div className="App">
+    <div className='App'>
+      <h1>ToDoApp React/TypeScript</h1>
+
+      <FormControl>
+        <TextField
+          label='Just Do It'
+          value={input}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            setInput(e.target.value);
+          }}
+        />
+      </FormControl>
+
+      <button disabled={!input} onClick={NewTask}>
+        <AddToPhotosIcon />
+      </button>
+
+      {tasks.map((task) => (
+        <h3 key={task.id}>{task.title}</h3>
+      ))}
     </div>
-  )
-}
+  );
+};
 
 export default App;
--- src/firebase.ts
+++ src/firebase.ts
@@ -1,16 +1,16 @@
-import  firebase from "firebase/app";
-import "firebase/app"
-import "firebase/firestore"
-import "firebase/autn"
+import firebase from 'firebase/app';
+import 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 
-export const firebaseApp=firebase.initializeApp({
-    apiKey:process.env.REACT_APP_FIREBASE_APIKEY,
-    authDomain:process.env.REACT_APP_FIREBASE_DOMAIN,
-    projectId:process.env.REACT_APP_FIREBASE_PROJECT,
-    storageBucket:process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId:process.env.REACT_APP_FIREBASE_SENDER_ID,
-    appId:process.env.REACT_APP_FIREBASE_APP_ID,
-})
+export const firebaseApp = firebase.initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+});
 
-export const db=firebaseApp.firestore();
-export const auth=firebase.auth()
+export const db = firebaseApp.firestore();
+export const auth = firebase.auth();
